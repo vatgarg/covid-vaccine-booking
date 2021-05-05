f7deef20525c68764bc4d94987c357fec57b1c77
vatgarg <57964020+vatgarg@users.noreply.github.com>
2021-05-05 20:06:47 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -24,6 +24,10 @@ def beep(freq, duration):
     elif platform == "win32":
         winsound.Beep(freq, duration)
     else:
+        # install sox on linux. Tested.
+        for i in range(5):
+            os.system('play -q -n synth 0.1 sin 880 || echo -e "\a"')
+            time.sleep(2)
         print("JEEEELOOOO!!!")
 
 
